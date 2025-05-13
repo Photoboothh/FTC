c1564611033be0d9fbd1fe52a3de37b17af5d3cb
Bảo <phungthienbao2009@gmail.com>
2025-05-13 19:43:49 +0700

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/TeleOp.java
@@ -6,7 +6,15 @@ import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
 public class TeleOp extends LinearOpMode {
     @Override
     public void runOpMode() {
-        Drivetrain Drive = new Drivetrain();
-        Drive.runOpMode();
+        Drivetrain drive = new Drivetrain(this);
+        drive.init();
+
+        waitForStart();;
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            drive.Drive(gamepad1.left_stick_y,gamepad1.left_stick_x,gamepad1.right_stick_x);
+        }
     }
 }
